66088f3cd70cb2cf81b80fcbe88e0ab80d0e3a03
Julian Gendritzki <jimbo.gend@icloud.com>
2025-09-08 11:30:05 +0200

    Ich habe die Challenge fertig

--- Reshaping_Challenge.py
+++ Reshaping_Challenge.py
@@ -3,7 +3,14 @@ import numpy as np
 a = np.arange(1, 13)
 
 newarr = a.reshape(3, 4)
-print(f"3x4: \n{newarr}")
+print(f"3x4: \n{newarr}\n")
 
-splitarr = np.array_split(a, 2)
-print(splitarr)
+left = newarr[:, :2]
+right = newarr[:, 2:]
+print("Split:")
+print(left)
+print(right)
+print("")
+
+stackarr = a.reshape(6, 2)
+print(f" Gestackt: \n {stackarr}")
